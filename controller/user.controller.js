c7e7e29a022e8c0380f90b002f4a4f5f26cf0ba4
nasim-coder <khannasimahmad@gmail.com>
2023-11-02 18:36:44 +0530

    crated register api

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,14 @@
-
+const { user } = require('../models/index')
+const {hash, compare} = require('bcrypt');
 exports.register = async (req, res)=>{
-
+    const { firstName, lastName, email, password } = req.body;
+    const userFound = await user.findOne({where:{ email}});
+    if(userFound){
+        return res.status(200).send({success: false, message: 'email already exist'});
+    }
+    const hashedpassword = await hash(password, 10);
+    const created = await user.create({firstName, lastName, email, password: hashedpassword});
+    res.status(201).send({success: true, message: 'registered successfully'});
 }
 
 exports.signin = async (req, res)=>{
