2473fe13733c2eca563f5196d538805afac444e5
nasim-coder <khannasimahmad@gmail.com>
2023-11-02 09:55:55 +0530

    created user controller

---
+++ controller/user.controller.js
@@ -0,0 +1,16 @@
+
+exports.register = async (req, res)=>{
+
+}
+
+exports.signin = async (req, res)=>{
+
+}
+
+exports.userList = async (req, res)=>{
+
+}
+
+exports.userDetails = async (req, res)=>{
+
+}
