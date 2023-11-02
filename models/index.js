87c899d6c52e0bac4313d6dd40b3a7776ff9f42f
nasim-coder <khannasimahmad@gmail.com>
2023-11-02 09:20:18 +0530

    setup sequelize, created model user

--- models/index.js
+++ models/index.js
@@ -2,9 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
+const Sequelize = require('sequelize')
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
@@ -16,28 +14,16 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.user = require('./user')(sequelize, Sequelize);
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3333;
+const db = require('./models/index');
 
-
+// db.sequelize
+//   .sync({ alter: true })
+//   .then(() => {
+//     console.log("db altered successfully");
+//   })
+//   .catch((err) => {
+//     console.log("err//or", err); 
+//   });
 
 app.listen(PORT, ()=>console.log(`server is running on PORT: ${PORT}`));
