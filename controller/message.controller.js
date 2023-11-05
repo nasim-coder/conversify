2bc8f51523b67767204eae0c77ad94a7e4d8ed92
nasim-coder <khannasimahmad@gmail.com>
2023-11-05 20:55:17 +0530

    create message route and controller

---
+++ controller/message.controller.js
@@ -0,0 +1,18 @@
+
+exports.sendMessage = async (req, res)=>{
+    const {group_id, reciver_id} = req.query;
+    const {id} = req.data;
+}
+
+
+exports.retrieveGroupMessages = async (req, res)=>{
+
+}
+
+exports.conversation = async (req, res)=>{
+
+}
+
+exports.deleteMessage = async (req, res)=>{
+
+}
--- route/message.route.js
+++ route/message.route.js
@@ -1,5 +1,20 @@
 const { Router } = require('express');
 const messageRoute = Router();
+const controller = require('../controller/message.controller');
 
+messageRoute.post('/create-message', controller.sendMessage)
+messageRoute.get('/group-messages', controller.retrieveGroupMessages);
+messageRoute.get('/conversation', controller.conversation);
+messageRoute.delete('/delete-message', controller.deleteMessage);
 
 module.exports = messageRoute;
+
+/*
+Message Management:
+
+POST /api/message: Send a message to a user or a group.
+GET /api/message/:messageId: Retrieve message details.
+DELETE /api/message/:messageId: Delete a message (sender or admin-only).
+GET /api/user/:userId/messages: Retrieve a user's messages.
+GET /api/group/:groupId/messages: Retrieve messages within a group.
+*/
